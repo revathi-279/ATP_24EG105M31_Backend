d4436d27cdaf1518c34a121afd62dcbf0dfe3efc
Revathi <revathi729070@gmail.com>
2026-05-09 17:27:29 +0530

    Test verification

--- middlewares/VerifyToken.js
+++ middlewares/VerifyToken.js
@@ -3,30 +3,37 @@ import { config } from 'dotenv'
 const { verify } = jwt
 config()
 
-
 export const verifyToken = (...allowedRoles) => {
     return (req, res, next) => {
         try {
-            //get token from cookie
+
+            console.log("COOKIES:", req.cookies);
+            console.log("TOKEN:", req.cookies?.token);
+            console.log("SECRET:", process.env.SECRET_KEY);
+
             const token = req.cookies?.token
-            //check token existed or not
+
             if (!token) {
                 return res.status(401).json({ message: "Please login first" })
             }
-            //validate token(decode the token)
+
             let decodedToken = verify(token, process.env.SECRET_KEY)
 
-            //check if the role is same as in decoded token
+            console.log("DECODED:", decodedToken);
+
             if (!allowedRoles.includes(decodedToken.role)) {
                 return res.status(403).json({ message: "You are not authorized" })
             }
-            //add decoded token
+
             req.user = decodedToken
-            //call next
+
             next()
+
         } catch (err) {
+
+            console.log("VERIFY ERROR:", err);
+
             res.status(401).json({ message: "Invalid token" })
         }
     }
-
 }
